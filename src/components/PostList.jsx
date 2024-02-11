cdb5fb9d23ef2286d77e41758458519a4453ece2
Muzamil Pirzada <muzmailali34@gmail.com>
2024-02-11 19:38:27 +0500

    dummy api fetch progress

--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -3,13 +3,20 @@ import { useContext } from "react";
 import Post from "./Post";
 // as is for to change any component name chage
 import { PostList as PostlistData } from "../store/post-list-store";
+import WelcomeMessage from "./WelcomeMessage";
 
 const PostList = () => {
   // we want only postlist value from PostlistData
   const { postList } = useContext(PostlistData);
-  // console.log(postList[1].id)
+
+  const handleGetPostsClick = () => {
+    fetch("https://dummyjson.com/posts")
+      .then((res) => res.json())
+      .then(console.log);
+  };
   return (
     <>
+      {postList.length === 0 && <WelcomeMessage onGetPostsClick={handleGetPostsClick}></WelcomeMessage>}
       {postList.map((post) => (
         // eslint-disable-next-line react/jsx-key
         <Post key={post.id} post={post} />
--- src/store/post-list-store.jsx
+++ src/store/post-list-store.jsx
@@ -23,10 +23,7 @@ const postListReducer = (currPostList, action) => {
 //   this is a component that takes chilfren as a props and it retrun PostList.Provider
 // eslint-disable-next-line no-unused-vars
 const PostListProvider = ({ children }) => {
-  const [postList, dispatchPostList] = useReducer(
-    postListReducer,
-    DEFAULT_POST_LIST
-  );
+  const [postList, dispatchPostList] = useReducer(postListReducer, []);
   const addPost = (userId, postTitle, postBody, reactions, tags) => {
     dispatchPostList({
       type: "ADD_POST",
@@ -39,7 +36,6 @@ const PostListProvider = ({ children }) => {
         tags: tags,
       },
     });
-    
   };
 
   const deletePost = (postId) => {
@@ -63,24 +59,5 @@ const PostListProvider = ({ children }) => {
     </PostList.Provider>
   );
 };
-
-const DEFAULT_POST_LIST = [
-  {
-    id: "1",
-    title: "Going to hyderbad",
-    body: "Hi Frirend, Iam going to hyderabad for some days",
-    reaction: 2,
-    userId: "user-9",
-    tags: ["vacation", "Mumbai", "Enjoying"],
-  },
-  {
-    id: "10",
-    title: "Going to hyderbad",
-    body: "Hi Frirend, Iam going to hyderabad for some days",
-    reaction: 6,
-    userId: "user-10",
-    tags: ["vacation", "Mumbai", "Enjoying"],
-  },
-];
 
 export default PostListProvider;
